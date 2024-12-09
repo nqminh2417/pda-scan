9d5dd032a008f844fa73682afee586336cc5b75b
nqminh2417 <quangminh2417@gmail.com>
2024-12-09 09:04:09 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my_pda_scan
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/my_pda_scan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.my_pda_scan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
